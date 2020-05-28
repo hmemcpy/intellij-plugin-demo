5df9b53bc9a6754e7cb732b142b899d3ecae95f5
Igal Tabachnik <hmemcpy@gmail.com>
2020-05-28 14:40:06 +0300

    Complete source code

--- src/main/scala/inspections/DetectUnusedExpressionsInspection.scala
+++ src/main/scala/inspections/DetectUnusedExpressionsInspection.scala
@@ -4,6 +4,7 @@ import com.intellij.codeInspection.{InspectionManager, LocalQuickFix, ProblemDes
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.codeInspection.AbstractRegisteredInspection
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 
 class DetectUnusedExpressionsInspection extends AbstractRegisteredInspection {
 
@@ -13,5 +14,21 @@ class DetectUnusedExpressionsInspection extends AbstractRegisteredInspection {
     descriptionTemplate: String,
     highlightType: ProblemHighlightType
   )(implicit manager: InspectionManager, isOnTheFly: Boolean): Option[ProblemDescriptor] =
-    ???
+    element match {
+      case expr: ScExpression =>
+        (expr, Option(expr.getNextSiblingNotWhitespace)) match {
+          case (zioRef(_, _), Some(zioRef(_, _))) =>
+            Some(
+              manager.createProblemDescriptor(
+                expr,
+                "This expression is unused. Did you mean to compose it with another effect?",
+                isOnTheFly,
+                Array.empty[LocalQuickFix],
+                ProblemHighlightType.LIKE_UNUSED_SYMBOL
+              )
+            )
+          case _ => None
+        }
+      case _ => None
+    }
 }
--- src/main/scala/inspections/SimplifyMapInspection.scala
+++ src/main/scala/inspections/SimplifyMapInspection.scala
@@ -7,15 +7,36 @@ class SimplifyMapInspection extends OperationOnCollectionInspection {
   setLikeCollectionClasses(Array("zio._"))
 
   override def possibleSimplificationTypes: Array[SimplificationType] =
-    Array(AsSimplification)
+    Array(AsSimplification, ExitCodeSimplification)
 }
 
 object AsSimplification extends SimplificationType {
   override def hint: String = "Replace with .as"
 
   override def getSimplification(expr: ScExpression): Option[Simplification] =
-    ???
+    // qualifier.map(_ => x) -> x
+    expr match {
+      case qual `.map` `_ => x`(x) => Some(replacement(expr, qual, x))
+      case _                       => None
+    }
 
   private def replacement(expr: ScExpression, qual: ScExpression, body: ScExpression) =
     replace(expr).withText(invocationText(qual, s"as(${body.getText}"))
+}
+
+object ExitCodeSimplification extends SimplificationType {
+  override def hint: String = "Replace with .exitCode"
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] =
+    // qualifier.map(_ => x) -> x
+    expr match {
+      case qual `.map` `_ => x`(x)
+          if x.textMatches("ExitCode.success") ||
+            x.textMatches("ExitCode.failure") =>
+        Some(replacement(expr, qual))
+      case _ => None
+    }
+
+  private def replacement(expr: ScExpression, qual: ScExpression) =
+    replace(expr).withText(invocationText(qual, s"exitCode"))
 }
